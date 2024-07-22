f2b761f5172713b7fa207e848edab95f49fc7e13
FacundoBlanco <facundoblancov@gmail.com>
2024-07-22 16:43:16 -0300

    spread and rest

--- ecmascript-course/src/es6/04-rest-spread.js
+++ ecmascript-course/src/es6/04-rest-spread.js
@@ -7,3 +7,19 @@ console.log(a, fruits[1]);
 let user = {username: 'John', age: 30};
 let { username, age } = user;
 console.log(username, age);
+
+// spread operator
+let person = {name: 'Pepe', age: 30};
+let country = 'Uruguay';
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(2, 3, 4, 5); // 5
