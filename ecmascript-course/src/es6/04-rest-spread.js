9fe88a340d5f4a17ff4d2f67156cb27e4115dd68
FacundoBlanco <facundoblancov@gmail.com>
2024-07-22 10:35:43 -0300

    Destructuring

---
+++ ecmascript-course/src/es6/04-rest-spread.js
@@ -0,0 +1,9 @@
+// arrays destructuring
+let fruits = ['apple', 'banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// objects destructuring
+let user = {username: 'John', age: 30};
+let { username, age } = user;
+console.log(username, age);
