1e5b1c76aae35757526b2daf152225053f41a425
FacundoBlanco <facundoblancov@gmail.com>
2024-07-23 09:57:55 -0300

    Classes

---
+++ ecmascript-course/src/es6/classes.js
@@ -0,0 +1,39 @@
+// Define a class
+class user {
+  // Constructor
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  // Methods
+  speak() {
+    return `${this.greeting()}, my name is ${this.name}`;
+  }
+  greeting() {
+    return 'Hello';
+  }
+
+  get uAge() {
+    return this.age;
+  }
+  set uAge(age) {
+    this.age = age;
+  }
+}
+
+const facu = new user('facu');
+console.log(facu.greeting());
+
+const developer = new user('developer');
+console.log(developer.greeting());
+
+const pepe = new user('pepe');
+console.log(pepe.greeting());
+
+console.log(facu.speak());
+console.log(developer.speak());
+console.log(pepe.speak());
+
+const pablo = new user('pablo', 32);
+console.log(pablo.uAge)
+console.log(pablo.uAge = 33)
