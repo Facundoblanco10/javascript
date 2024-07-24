b9338e47fff80b50c824424badcf405eacd3f083
FacundoBlanco <facundoblancov@gmail.com>
2024-07-24 09:38:14 -0300

    ES 7

---
+++ ecmascript-course/src/es7/01-array-includes.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.includes(3)); // true
+console.log(numbers.includes(6)); // false
+
+const list = ["apple", "banana", "kiwi", "orange"];
+
+console.log(list.includes("banana")); // true
+console.log(list.includes("grapes")); // false
+console.log(list.includes("Banana")); // false
